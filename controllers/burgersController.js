ff943328e77be4b44147f340baa1e2a28a8d4746
Kyle Knox <kyleknox86@gmail.com>
2019-06-20 14:39:25 -0600

    sequelize

--- controllers/burgersController.js
+++ controllers/burgersController.js
@@ -1,63 +1,61 @@
-var express = require("express");
+// Import the model to use its database functions.
+var db = require("../models/");
 
-var router = express.Router();
+module.exports = function (app) {
 
-// Import the model (burger.js) to use its database functions.
-var burger = require("../models/burger.js");
-
-// Create all our routes and set up logic within those routes where required.
-router.get("/", function (req, res) {
-    burger.all(function (data) {
-        let hbsObj = {
-            burgers: data
-        }
-        res.render("index", hbsObj);
-    })
-});
-
-
-
-router.post("/api/burgers", function (req, res) {
-    burger.create(req.body.name, req.body.devoured, function (result) {
-        res.json({
-            id: result.insertId
-        })
-    })
-});
-
-
-
-router.put("/api/burgers/:id", function (req, res) {
-    let condition = "id = " + req.params.id;
-    burger.update({
-        devoured: req.body.devoured
-    }, condition, function (result) {
-        if (result.changedRows == 0) {
-            // If no rows were changed, then the ID must not exist, so 404
-            return res.status(404).end();
-        } else {
-            res.status(200).end();
-        }
+    // GET route for getting all of the burgers
+    app.get("/", function (req, res) {
+        console.log("request");
+        // findAll returns all entries for a table when used with no options
+        db.Burger.findAll({}).then(function (burger) {
+            // We have access to the burgers as an argument inside of the callback function
+            res.json(burger);
+        });
     });
-});
 
-
-
-router.delete("/api/burgers/:id", function (req, res) {
-    let condition = "id = " + req.params.id;
-
-    burger.delete(condition, function (result) {
-        if (result.affectedRows == 0) {
-            // If no rows were changed, then the ID must not exist, so 404
-            return res.status(404).end();
-        } else {
-            res.status(200).end();
-        }
+    // POST route for saving a new burger
+    app.post("/api/burgers", function (req, res) {
+        // create takes an argument of an object describing the item we want to
+        // insert into our table. In this case we just we pass in an object with a text
+        // and complete property
+        db.Burger.create({
+            burger_name: req.body.text,
+            devoured: req.body.complete
+        }).then(function (burger) {
+            // We have access to the new burger as an argument inside of the callback function
+            res.json(burger);
+        });
     });
-});
+    // DELETE route for deleting todos. We can get the id of the todo to be deleted
+    // from req.params.id
+    app.delete("/api/burgers/:id", function (req, res) {
+        // Destroy takes in one argument: a "where object describing the todos we want to destroy
+        db.Burger.destroy({
+                where: {
+                    id: req.params.id
+                }
+            })
+            .then(function (burger) {
+                res.json(burger);
+            });
 
+    });
 
+    // PUT route for updating todos. We can get the updated todo data from req.body
+    app.put("/api/burgers", function (req, res) {
+        // Update takes in two arguments, an object describing the properties we want to update,
+        // and another "where" object describing the todos we want to update
+        db.Burger.update({
+                burger_name: req.body.text,
+                devoured: req.body.complete
+            }, {
+                where: {
+                    id: req.body.id
+                }
+            })
+            .then(function (burger) {
+                res.json(burger);
+            });
 
-
-// Export routes for server.js to use.
-module.exports = router;
+    });
+};
--- models/burger.js
+++ models/burger.js
@@ -1,27 +1,17 @@
-const orm = require("../config/orm.js");
-
-const burger = {
-    all: function (cb) {
-        orm.all("burgers", function (res) {
-            cb(res);
-        });
-    },
-    // The variables cols and vals are arrays.
-    create: function (burger_name, devoured, cb) {
-        orm.create(burger_name, devoured, function (res) {
-            cb(res);
-        });
-    },
-    update: function (objColVals, condition, cb) {
-        orm.update("burgers", objColVals, condition, function (res) {
-            cb(res);
-        });
-    },
-    delete: function (condition, cb) {
-        orm.delete("burgers", condition, function (res) {
-            cb(res);
-        });
-    }
+module.exports = function (sequelize, dataTypes) {
+    var Burger = sequelize.define('burgers', {
+        // Here are the columns of the table
+        id: {
+            type: dataTypes.INTEGER,
+            primaryKey: true
+        },
+        burger_name: {
+            type: dataTypes.STRING
+        },
+        devoured: {
+            type: dataTypes.BOOLEAN,
+            defaultValue: false
+        },
+    })
+    return Burger;
 };
-// Export the database functions for the controller (burgersController.js).
-module.exports = burger;
